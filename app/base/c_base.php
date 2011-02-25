231f4416165b757c25b36d7a33d32d2cb7b69121
David Brännvall <david.brannvall@retrolink.se>
2011-02-25 13:25:30 +0100

    default meta description added to base_controller

--- app/base/c_base.php
+++ app/base/c_base.php
@@ -4,7 +4,8 @@ class base_controller extends uf_controller
   public function before_action()
   {
     parent::before_action();
-    $this->caller()->meta_description = 'poo';
+    $this->caller()->meta_description = 'UMVC';
+    $this->caller()->meta_keywords = 'php, mvc, framework, web development';
   }
 }
 ?>
