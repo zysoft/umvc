b7e9113d513c6f7912be43e474c091f37fd73957
David Brännvall <david.brannvall@retrolink.se>
2011-03-03 16:20:58 +0100

    minor cosmetic fix

--- app_demo/modules/language/view/v_index.php
+++ app_demo/modules/language/view/v_index.php
@@ -1,6 +1,6 @@
 <h1><?=$language['base']['menu']['language'].': '.$language['language']['choose_language']?></h1>
 
-<p>
-  <a href="/language/set?language=sv_SE">Svenska</a><br />
-  <a href="/language/set?language=en_US">English</a>
-</p>
+<ul>
+  <li><a href="/language/set?language=en_US">English</a></li>
+  <li><a href="/language/set?language=sv_SE">Svenska</a></li>
+</ul>
--- config/config.original.php
+++ config/config.original.php
@@ -1,6 +1,7 @@
 <?
 
 $uf_config = array(
+  'language'   => 'sv_SE',
   'always_bake' => FALSE,
   'load_propel' => TRUE,
   'app_dir'     => '/app_demo'
