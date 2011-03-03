da1ce71967daba1e1bf7c9a27f300dd731684070
David Brännvall <david.brannvall@retrolink.se>
2011-03-03 16:31:44 +0100

    fixed redirect bug in language module

--- app_demo/modules/language/c_language.php
+++ app_demo/modules/language/c_language.php
@@ -1,5 +1,4 @@
 <?
-
 class language_controller extends base_controller
 {
   public function index()
@@ -10,7 +9,7 @@ class language_controller extends base_controller
   public function set()
   {
     uf_session::set('language',$this->request()->parameter('language','en_US'));
-    $this->response()->header('Location','http://'.$_SERVER['SERVER_NAME'].'/language');
+    $this->response()->header('Location','http://'.$_SERVER['SERVER_NAME'].($_SERVER['SERVER_PORT'] == 80 ? '' : ':'.$_SERVER['SERVER_PORT']).'/language');
     return FALSE;
   }
 
