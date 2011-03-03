598cd1f065bf8d3e6154f7f52a96446f05b88c0d
David Brännvall <david.brannvall@retrolink.se>
2011-03-03 16:26:59 +0100

    fixed redirect in language module

--- app_demo/modules/language/c_language.php
+++ app_demo/modules/language/c_language.php
@@ -10,7 +10,7 @@ class language_controller extends base_controller
   public function set()
   {
     uf_session::set('language',$this->request()->parameter('language','en_US'));
-    $this->response()->header('Location','http://umvc.dev/language');
+    $this->response()->header('Location','http://'.$_SERVER['SERVER_NAME'].'/language');
     return FALSE;
   }
 
