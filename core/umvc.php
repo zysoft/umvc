f3843a8bff2a27e54d7caf6e2bc75a33b751dcf7
jaw <jaw@jaw-front.dev.hrnorth.net>
2011-03-18 12:03:57 +0100

    fixed propel inclusion errors

--- core/umvc.php
+++ core/umvc.php
@@ -10,15 +10,4 @@ require_once(UF_BASE.'/core/request.php');
 require_once(UF_BASE.'/core/httprequest.php');
 require_once(UF_BASE.'/core/controller.php');
 
-if(uf_application::config('load_propel'))
-{
-  // Initialize Propel with the runtime configuration
-  require_once UF_BASE.'/propel/propel-1.5.6/runtime/lib/Propel.php';
-
-  Propel::init(uf_application::app_dir().'/data/build/conf/umvc-conf.php');
-
-  // Add the generated 'classes' directory to the include path
-  set_include_path('/path/to/bookstore/build/classes'.PATH_SEPARATOR.get_include_path());  
-}
-
 ?>
