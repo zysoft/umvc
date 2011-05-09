87e54c6276735041151208023e144f004d49eb19
David Brännvall <david.brannvall@retrolink.se>
2011-05-09 15:55:49 +0200

    removed reference to core/request.php

--- core/umvc.php
+++ core/umvc.php
@@ -6,7 +6,6 @@ require_once(UF_BASE.'/core/application.php');
 require_once(UF_BASE.'/core/baker.php');
 require_once(UF_BASE.'/core/session.php');
 require_once(UF_BASE.'/core/response.php');
-require_once(UF_BASE.'/core/request.php');
 require_once(UF_BASE.'/core/httprequest.php');
 require_once(UF_BASE.'/core/controller.php');
 
