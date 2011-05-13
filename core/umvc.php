e00f3b3077442ee9a15ed455f30224099ac96002
David Brännvall <david.brannvall@retrolink.se>
2011-05-13 10:43:10 +0200

    disabed magic quotes

--- core/umvc.php
+++ core/umvc.php
@@ -1,6 +1,23 @@
 <?
 error_reporting(E_ALL);
 
+// http://www.php.net/manual/en/security.magicquotes.disabling.php
+if (get_magic_quotes_gpc()) {
+    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+    while (list($key, $val) = each($process)) {
+        foreach ($val as $k => $v) {
+            unset($process[$key][$k]);
+            if (is_array($v)) {
+                $process[$key][stripslashes($k)] = $v;
+                $process[] = &$process[$key][stripslashes($k)];
+            } else {
+                $process[$key][stripslashes($k)] = stripslashes($v);
+            }
+        }
+    }
+    unset($process);
+}
+
 include_once(UF_BASE.'/core/debug.php');
 require_once(UF_BASE.'/core/application.php');
 require_once(UF_BASE.'/core/baker.php');
@@ -10,4 +27,4 @@ require_once(UF_BASE.'/core/httprequest.php');
 require_once(UF_BASE.'/core/controller.php');
 require_once(UF_BASE.'/core/validator.php');
 
-?>
+/* EOF */
