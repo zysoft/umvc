1d73756cb4feeee2b5935df08b08bde528376ded
David Brännvall <david.brannvall@retrolink.se>
2011-08-03 09:56:33 +0200

    small fix in form validation demo

--- app_demo/lib/js/b_form_validation.js
+++ app_demo/lib/js/b_form_validation.js
@@ -1,6 +1,7 @@
 umvc.bind("umvc.validator.error", function(event, data) {
-  alert(data.message);
-  $("form#" + data.form_id + " *[name="+data.name+"]").addClass("error");
+  $("form#" + data.form_id + " *[name="+data.name+"]")
+    .addClass("error")
+    .next()[0].innerHTML = data.message;
 });
 
 umvc.bind("umvc.validator.success", function(event, data) {
--- app_demo/modules/examples/view/v_form_validation.php
+++ app_demo/modules/examples/view/v_form_validation.php
@@ -1,13 +1,13 @@
 <p>Hint: the password is 'pw'</p>
 <form id="validator_form" action="" method="post">
-  <p><strong>email</strong><br /><input type="text" name="email" value="<?=htmlentities($uf_request->parameter('email'))?>" /></p>
-  <p><strong>password</strong><br /><input type="password" name="password" value="" /></p>
+  <p><strong>email</strong><br /><input type="text" name="email" value="<?=htmlentities($uf_request->parameter('email'))?>" /> <span></span></p>
+  <p><strong>password</strong><br /><input type="password" name="password" value="" /> <span></span></p>
   <p><input type="submit" name="submit" value="Login" /></p>
 </form>
 
 <p>Dummy form</p>
 <form action="" method="post">
-  <p><strong>email</strong><br /><input type="text" name="email" value="<?=htmlentities($uf_request->parameter('email'))?>" /></p>
-  <p><strong>password</strong><br /><input type="password" name="password" value="" /></p>
+  <p><strong>email</strong><br /><input type="text" name="email" value="<?=htmlentities($uf_request->parameter('email'))?>" /> <span></span></p>
+  <p><strong>password</strong><br /><input type="password" name="password" value="" /> <span></span></p>
   <p><input type="submit" name="submit" value="Login" /></p>
 </form>
