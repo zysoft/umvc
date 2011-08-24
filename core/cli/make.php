5830f6e526a049d0c0fc39f043482aafd7b1d4e7
David Brännvall <david.brannvall@retrolink.se>
2011-08-24 16:52:38 +0200

    added reverse function from txt to array

--- core/cli/make.php
+++ core/cli/make.php
@@ -12,13 +12,11 @@ function scan_namespace($dir)
   $index = count($path);
   $files = Make::get_files_from_dir($dir);
   $files = Make::filter($files, 'filter_php_file');
-  
   foreach($files as &$file)
   {
     $file = implode('/', array_slice($file, $index + 1));
   }
   $files = array_flip($files);
-
   foreach($files as $file => &$ids)
   {
     $filename = UF_BASE.'/'.$dir.'/'.$file;
@@ -27,8 +25,10 @@ function scan_namespace($dir)
     if(preg_match_all('/_\(["\'](.*?)["\']\)/msi', $code, $matches))
     {
       $ids[] = $matches[1][0];
-    }
+    }    
+    if(count($ids) == 0) unset($files[$file]);
   }
+
   return array($dir => $files);
 }
 
@@ -56,27 +56,70 @@ function merge_namespaces($a, $b)
   return $b;
 }
 
+
 function pretty_print_namespace($namespaces)
 {
+  $output = '';
   if(count($namespaces) == 0) return;
   foreach($namespaces as $name => $namespace)
   {
-    echo 'namespace='.$name."\n";
+    $output .= 'namespace='.$name."\n";
     foreach($namespace as $file => $ids)
     {
       if(count($ids) > 0)
       {
-        echo "\n".'#'.$name.'/'.$file."\n";
+        $output .=  "\n".'#'.$file."\n";
         foreach($ids as $id)
         {
-          echo $id.'='."\n";
+          $output .=  $id.'='."\n";
         }        
       }
     }
-    echo "\n";      
-  }  
+    $output .=  "\n";      
+  }
+  return $output;
 }
 
+function namespaces_to_array($str)
+{
+  $namespace = '';
+  $file = '';
+  $a = array_map('trim', explode("\n", $str));
+  $result = array();
+  foreach($a as $l)
+  {
+    if($l != '')
+    {
+      if(($p = strpos($l, '=')) !== FALSE)
+      {
+        $keyval = explode('=', $l);
+        if($keyval[0] == 'namespace')
+        {
+          $namespace = $keyval[1];
+          if(!isset($result[$namespace]))
+          {
+            $result[$namespace] = array();
+          }
+        } else
+        {
+          $id = $keyval[0];
+          if(!isset($result[$namespace][$file]))
+          {
+            $result[$namespace][$file] = array();
+          }
+          $result[$namespace][$file][] = $id;
+        }
+      } else
+      if(strpos($l, '#') === 0)
+      {
+        $file = substr($l, 1);
+      }
+    }
+  }
+  return $result;
+}
+/*
+*/
 $lib = scan_namespace('app_demo/lib');
 $modules = scan_namespaces('app_demo/modules');
 $site_base = scan_namespaces('app_demo/sites/hosts/FALLBACK/base');
@@ -86,6 +129,16 @@ $namespaces = merge_namespaces($lib, $modules);
 $namespaces = merge_namespaces($namespaces, $site_modules);
 $namespaces = merge_namespaces($namespaces, $site_base);
 
-pretty_print_namespace($namespaces);
+echo "-- input --\n";
+print_r($namespaces);
+
+echo "-- pretty --\n";
+$pretty = pretty_print_namespace($namespaces);
+print_r($pretty);
+
+echo "-- output --\n";
+$output = namespaces_to_array($pretty);
+print_r($output);
+
 
 /* EOF */
